a86553bddbf6b1151678f5f8267c09bfd307fb88
nicolaslarsen <nicolasringsmose@hotmail.com>
2018-02-26 15:20:47 +0100

    Added files for statistics and graphs

---
+++ evd-test/Statistic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evd_test
+{
+    public class Statistic<T> where T: Evalue, new()
+    {
+        private List<T> FirstFile;
+        private List<T> SecondFile;
+        
+        public Statistic(List<T> firstFile, List<T> secondFile)
+        {
+            FirstFile = firstFile;
+            SecondFile = secondFile;
+        } 
+
+        public int BuildStats()
+        {
+            string output = "";
+
+            foreach(T Ejendom in FirstFile)
+            {
+                
+            }
+
+            return 0;
+        }
+    }
+}
